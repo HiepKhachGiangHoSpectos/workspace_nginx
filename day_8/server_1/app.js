69bc22a18a7eb2b72849a1abf4b3b824d02277ba
BaoQuanHan <hieu.tran@pionero.io>
2025-09-25 13:16:25 +0700

    tinh huong 2: (dat van de)

--- day_8/server_1/app.js
+++ day_8/server_1/app.js
@@ -5,6 +5,24 @@ app.get('/', (req, res) => {
     res.send('Hello from Server 1');
 });
 
+// route chậm
+app.get('/slow', (req, res) => {
+    console.log(`Received request at ${new Date().toISOString()} on Server 1`);
+    setTimeout(() => {
+        console.log(`Sending response at ${new Date().toISOString()} on Server 1`);
+        res.send('Server 1 took 15 seconds');
+    }, 15000);
+});
+
+// route CPU nặng
+app.get('/cpu', (req, res) => {
+    // giả lập CPU bận
+    let now = Date.now() + 15000; // 15s
+    while (Date.now() < now) {
+    }
+    res.send('CPU server 2 busy done!');
+});
+
 const PORT = 8081;
 app.listen(PORT, () => {
     console.log(`Server 1 running on port ${PORT}`);
--- day_8/server_2/app.js
+++ day_8/server_2/app.js
@@ -5,6 +5,24 @@ app.get('/', (req, res) => {
     res.send('Hello from Server 2');
 });
 
+// route chậm
+app.get('/slow', (req, res) => {
+    console.log(`Received request at ${new Date().toISOString()} on Server 2`);
+    setTimeout(() => {
+        console.log(`Sending response at ${new Date().toISOString()} on Server 2`);
+        res.send('Server 2 took 15 seconds');
+    }, 15);
+});
+
+// route CPU nặng
+app.get('/cpu', (req, res) => {
+    // giả lập CPU bận
+    let now = Date.now() + 15000; // 15s
+    while (Date.now() < now) {
+    }
+    res.send('CPU server 2 busy done!');
+});
+
 const PORT = 8082;
 app.listen(PORT, () => {
     console.log(`Server 2 running on port ${PORT}`);
