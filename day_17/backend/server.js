b772a6428b26032c9e8281e21f02d96ed9ec3e59
BaoQuanHan <hieu.tran@pionero.io>
2025-09-26 17:18:14 +0700

    DAY 17: Proxy cache & purge (cache step 4) - dung queue, retry, neu retry khong oke, luu vao db/file

--- day_17/backend/server.js
+++ day_17/backend/server.js
@@ -1,47 +1,48 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import Redis from 'ioredis';
+import express from "express";
+import { Kafka } from "kafkajs";
 
 const app = express();
-app.use(bodyParser.json());
-
-const redis = new Redis({
-  host: 'redis', // container name
-  port: 6379,
-});
+app.use(express.json());
 
 let users = {
-  1: { id: 1, name: 'Alice', age: 20 },
-  2: { id: 2, name: 'Bob', age: 25 },
+    "1": { id: 1, name: "Alice" },
+    "2": { id: 2, name: "Bob" },
 };
 
-// GET user (gateway sẽ cache kết quả)
-app.get('/user/:id', (req, res) => {
-  const { id } = req.params;
-  const user = users[id];
-  if (!user) return res.status(404).json({ error: 'User not found' });
-  res.json(user);
+// Kafka setup
+const kafka = new Kafka({ clientId: "backend", brokers: ["kafka:9092"] });
+const producer = kafka.producer();
+
+await producer.connect();
+
+// Publish cache invalidation
+async function publishCacheInvalidate(key) {
+    await producer.send({
+        topic: "cache_invalidate",
+        messages: [{ key, value: key }],
+    });
+    console.log("Published cache invalidation for", key);
+}
+
+// GET user
+app.get("/user/:id", (req, res) => {
+    const user = users[req.params.id];
+    if (!user) return res.status(404).json({ error: "User not found" });
+    console.log('============== ge user=========', user)
+    res.json(user);
 });
 
-// UPDATE user
-app.put('/user/:id', async (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
+// PUT user
+app.put("/user/:id", async (req, res) => {
+    const { id } = req.params;
+    const data = req.body;
+    if (!users[id]) return res.status(404).json({ error: "User not found" });
+    users[id] = { ...users[id], ...data };
 
-  if (!users[id]) return res.status(404).json({ error: 'User not found' });
+    // call Cache Invalidation
+    await publishCacheInvalidate(`user:${id}`);
 
-  users[id] = { ...users[id], ...data };
-
-  // Publish event cho Cache Invalidation Service
-  try {
-    await redis.publish('cache_invalidation', JSON.stringify({
-      key: `user:${id}`,
-    }));
-  } catch (err) {
-    console.error('Failed to publish cache invalidation', err);
-  }
-
-  res.json(users[id]);
+    res.json(users[id]);
 });
 
-app.listen(3000, () => console.log('Backend server listening on 3000'));
+app.listen(3000, () => console.log("Backend app running on port 3000"));
